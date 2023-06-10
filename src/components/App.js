67a7e53f19b71bb99e4402853a2bb8016f6cd947
abdel <ammegahe@amazon.com>
2023-06-10 10:52:08 -0400

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+// import Navbar from './Navbar';
+// import Home from './Home';
 function NavBar() {
   return (
     <nav>
@@ -17,6 +18,13 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About ()  {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+};
 
 
 function App() {
@@ -24,7 +32,9 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
